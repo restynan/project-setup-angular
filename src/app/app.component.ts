0c4f506dcc88913b48584af2ff3ee78e0aca8abd
restynan <restynan@gmail.com>
2020-10-24 11:09:09 -0400

    project-setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-setup';
+  showSecret= false;
+  onToggle(){
+    this.showSecret=!this.showSecret;
+
+  }
 }
